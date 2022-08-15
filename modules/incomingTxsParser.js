ebe4749a1b5d0afa6a6d0df1798c27657f31b338
adamant-al <adamantim@yandex.ru>
2022-08-15 22:54:34 +0300

    chore: add try-catches

--- modules/incomingTxsParser.js
+++ modules/incomingTxsParser.js
@@ -13,102 +13,106 @@ const notify = require('../helpers/notify');
 const historyTxs = {};
 
 module.exports = async (tx) => {
-  if (!tx) {
-    return;
-  }
+  try {
+    if (!tx) {
+      return;
+    }
 
-  if (historyTxs[tx.id]) { // do not process one tx twice
-    return;
-  }
+    if (historyTxs[tx.id]) { // do not process one tx twice
+      return;
+    }
 
-  const {IncomingTxsDb} = db;
-  const checkedTx = await IncomingTxsDb.findOne({txid: tx.id});
-  if (checkedTx !== null) {
-    return;
-  }
+    const {IncomingTxsDb} = db;
+    const checkedTx = await IncomingTxsDb.findOne({txid: tx.id});
+    if (checkedTx !== null) {
+      return;
+    }
 
-  log.log(`Received incoming transaction ${tx.id} from ${tx.senderId}.`);
+    log.log(`Received incoming transaction ${tx.id} from ${tx.senderId}.`);
 
-  let msg = '';
-  const chat = tx.asset.chat;
-  if (chat) {
-    msg = api.decodeMsg(chat.message, tx.senderPublicKey, config.passPhrase, chat.own_message).trim();
-  }
+    let msg = '';
+    const chat = tx.asset.chat;
+    if (chat) {
+      msg = api.decodeMsg(chat.message, tx.senderPublicKey, config.passPhrase, chat.own_message).trim();
+    }
 
-  if (msg === '') {
-    msg = 'NONE';
-  }
+    if (msg === '') {
+      msg = 'NONE';
+    }
 
-  // Parse social accounts from user message
-  const accounts = $u.getAccounts(msg);
+    // Parse social accounts from user message
+    const accounts = $u.getAccounts(msg);
 
-  let type = 'unknown';
-  if (msg.includes('_transaction') || tx.amount > 0) {
-    type = 'transfer'; // just for special message
-  } else if (accounts.notEmpty) {
-    type = 'check';
-  } else if (msg.startsWith('/')) {
-    type = 'command';
-  }
+    let type = 'unknown';
+    if (msg.includes('_transaction') || tx.amount > 0) {
+      type = 'transfer'; // just for special message
+    } else if (accounts.notEmpty) {
+      type = 'check';
+    } else if (msg.startsWith('/')) {
+      type = 'command';
+    }
 
-  // Check if we should notify about spammer, only once per 24 hours
-  const spamerIsNotify = await IncomingTxsDb.findOne({
-    sender: tx.senderId,
-    isSpam: true,
-    date: {$gt: (helpers.unix() - 24 * 3600 * 1000)}, // last 24h
-  });
-
-  const itx = new IncomingTxsDb({
-    _id: tx.id,
-    txid: tx.id,
-    date: helpers.unix(),
-    block_id: tx.blockId,
-    encrypted_content: msg,
-    accounts: accounts,
-    spam: false,
-    sender: tx.senderId,
-    type, // check, command, transfer or unknown
-    isProcessed: false,
-    isNonAdmin: false,
-  });
-
-  const countRequestsUser = (await IncomingTxsDb.find({
-    sender: tx.senderId,
-    date: {$gt: (helpers.unix() - 24 * 3600 * 1000)}, // last 24h
-  })).length;
-
-  if (countRequestsUser > 50 || spamerIsNotify) { // 50 per 24h is a limit for accepting commands, otherwise user will be considered as spammer
-    itx.update({
-      isProcessed: true,
+    // Check if we should notify about spammer, only once per 24 hours
+    const spamerIsNotify = await IncomingTxsDb.findOne({
+      sender: tx.senderId,
       isSpam: true,
+      date: {$gt: (helpers.unix() - 24 * 3600 * 1000)}, // last 24h
     });
-  }
 
-  await itx.save();
-  if (historyTxs[tx.id]) {
-    return;
-  }
-  historyTxs[tx.id] = helpers.unix();
+    const itx = new IncomingTxsDb({
+      _id: tx.id,
+      txid: tx.id,
+      date: helpers.unix(),
+      block_id: tx.blockId,
+      encrypted_content: msg,
+      accounts: accounts,
+      spam: false,
+      sender: tx.senderId,
+      type, // check, command, transfer or unknown
+      isProcessed: false,
+      isNonAdmin: false,
+    });
 
-  if (itx.isSpam && !spamerIsNotify) {
-    notify(`${config.notifyName} notifies _${tx.senderId}_ is a spammer or talks too much. Income ADAMANT Tx: https://explorer.adamant.im/tx/${tx.id}.`, 'warn');
-    const msgSendBack = `I’ve _banned_ you. You’ve sent too much transactions to me.`;
-    await api.sendMessageWithLog(config.passPhrase, tx.senderId, msgSendBack);
-    return;
-  }
+    const countRequestsUser = (await IncomingTxsDb.find({
+      sender: tx.senderId,
+      date: {$gt: (helpers.unix() - 24 * 3600 * 1000)}, // last 24h
+    })).length;
 
-  switch (type) {
-    case ('transfer'):
-      transferTxs(itx, tx);
-      break;
-    case ('check'):
-      checkTxs(itx, tx);
-      break;
-    case ('command'):
-      commandTxs(msg, tx, itx);
-      break;
-    default:
-      unknownTxs(tx, itx);
-      break;
+    if (countRequestsUser > 50 || spamerIsNotify) { // 50 per 24h is a limit for accepting commands, otherwise user will be considered as spammer
+      itx.update({
+        isProcessed: true,
+        isSpam: true,
+      });
+    }
+
+    await itx.save();
+    if (historyTxs[tx.id]) {
+      return;
+    }
+    historyTxs[tx.id] = helpers.unix();
+
+    if (itx.isSpam && !spamerIsNotify) {
+      notify(`${config.notifyName} notifies _${tx.senderId}_ is a spammer or talks too much. Income ADAMANT Tx: https://explorer.adamant.im/tx/${tx.id}.`, 'warn');
+      const msgSendBack = `I’ve _banned_ you. You’ve sent too much transactions to me.`;
+      await api.sendMessageWithLog(config.passPhrase, tx.senderId, msgSendBack);
+      return;
+    }
+
+    switch (type) {
+      case ('transfer'):
+        transferTxs(itx, tx);
+        break;
+      case ('check'):
+        checkTxs(itx, tx);
+        break;
+      case ('command'):
+        commandTxs(msg, tx, itx);
+        break;
+      default:
+        unknownTxs(tx, itx);
+        break;
+    }
+  } catch (e) {
+    log.error(`Error in ${helpers.getModuleName(module.id)} module: ${e}`);
   }
 };
--- modules/transferTxs.js
+++ modules/transferTxs.js
@@ -1,42 +1,48 @@
 const {SAT} = require('../helpers/const');
+const log = require('../helpers/log');
 const api = require('./api');
 const notify = require('../helpers/notify');
 const config = require('./configReader');
+const helpers = require('../helpers/utils');
 
 module.exports = async (itx, tx) => {
-  const msg = itx.encrypted_content;
-  let inCurrency;
-  let outCurrency;
-  let inAmountMessage;
+  try {
+    const msg = itx.encrypted_content;
+    let inCurrency;
+    let outCurrency;
+    let inAmountMessage;
 
-  if (tx.amount > 0) { // ADM income payment
-    inAmountMessage = tx.amount / SAT;
-    inCurrency = 'ADM';
-    outCurrency = msg;
-  } else if (msg.includes('_transaction')) { // not ADM income payment
-    inCurrency = msg.match(/"type":"(.*)_transaction/)[1];
-    try {
-      const json = JSON.parse(msg);
-      inAmountMessage = Number(json.amount);
-      outCurrency = json.comments;
-      if (outCurrency === '') {
-        outCurrency = 'NONE';
+    if (tx.amount > 0) { // ADM income payment
+      inAmountMessage = tx.amount / SAT;
+      inCurrency = 'ADM';
+      outCurrency = msg;
+    } else if (msg.includes('_transaction')) { // not ADM income payment
+      inCurrency = msg.match(/"type":"(.*)_transaction/)[1];
+      try {
+        const json = JSON.parse(msg);
+        inAmountMessage = Number(json.amount);
+        outCurrency = json.comments;
+        if (outCurrency === '') {
+          outCurrency = 'NONE';
+        }
+      } catch (e) {
+        inCurrency = 'none';
       }
-    } catch (e) {
-      inCurrency = 'none';
     }
+
+    outCurrency = String(outCurrency).toUpperCase().trim();
+    inCurrency = String(inCurrency).toUpperCase().trim();
+
+    // Validate
+    const msgSendBack = `I got a transfer from you. Thanks, bro.`;
+    const msgNotify = `${config.notifyName} got a transfer of ${inAmountMessage} ${inCurrency} from user ${tx.senderId}. I will not verify the transaction, check it manually. Income ADAMANT Tx: https://explorer.adamant.im/tx/${tx.id}.`;
+    const notifyType = 'log';
+
+    await itx.update({isProcessed: true}, true);
+
+    notify(msgNotify, notifyType);
+    await api.sendMessageWithLog(config.passPhrase, tx.senderId, msgSendBack);
+  } catch (e) {
+    log.error(`Error in ${helpers.getModuleName(module.id)} module: ${e}`);
   }
-
-  outCurrency = String(outCurrency).toUpperCase().trim();
-  inCurrency = String(inCurrency).toUpperCase().trim();
-
-  // Validate
-  const msgSendBack = `I got a transfer from you. Thanks, bro.`;
-  const msgNotify = `${config.notifyName} got a transfer of ${inAmountMessage} ${inCurrency} from user ${tx.senderId}. I will not verify the transaction, check it manually. Income ADAMANT Tx: https://explorer.adamant.im/tx/${tx.id}.`;
-  const notifyType = 'log';
-
-  await itx.update({isProcessed: true}, true);
-
-  notify(msgNotify, notifyType);
-  await api.sendMessageWithLog(config.passPhrase, tx.senderId, msgSendBack);
 };
